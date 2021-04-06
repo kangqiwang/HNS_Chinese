32bf0b4cd8dba13c0ca155d1857c64df451e9130
Kang Wang <kangqiwang@outlook.com>
2021-04-06 22:23:35 +0100

    delete WebClient code

--- src/main/java/com/kang/medicalwebapplication/webclient/MedicalWebCrawler.java
+++ src/main/java/com/kang/medicalwebapplication/webclient/MedicalWebCrawler.java
@@ -1,20 +1,20 @@
-package com.kang.medicalwebapplication.webclient;
-
-import org.springframework.http.MediaType;
-
-import org.springframework.web.reactive.function.client.ClientResponse;
-import org.springframework.web.reactive.function.client.WebClient;
-
-import reactor.core.publisher.Mono;
-
-public class MedicalWebCrawler {
-	private WebClient webClient = WebClient.create("https://api.nhs.uk");
-	
-//	private Mono<ClientResponse> responseMono = webClient.get().uri("").accept(MediaType.APPLICATION_JSON).header(headerName, headerValues).exchange();
-	
-//	public String getResult() {
-//        return ">> Result Mono<ClientResponse> = " + responseMono.flatMap(res -> res.bodyToMono(String.class)).block();
-//	}
-	
-	
-}
+//package com.kang.medicalwebapplication.webclient;
+//
+//import org.springframework.http.MediaType;
+//
+//import org.springframework.web.reactive.function.client.ClientResponse;
+//import org.springframework.web.reactive.function.client.WebClient;
+//
+//import reactor.core.publisher.Mono;
+//
+//public class MedicalWebCrawler {
+//	private WebClient webClient = WebClient.create("https://api.nhs.uk");
+//	
+////	private Mono<ClientResponse> responseMono = webClient.get().uri("").accept(MediaType.APPLICATION_JSON).header(headerName, headerValues).exchange();
+//	
+////	public String getResult() {
+////        return ">> Result Mono<ClientResponse> = " + responseMono.flatMap(res -> res.bodyToMono(String.class)).block();
+////	}
+//	
+//	
+//}
