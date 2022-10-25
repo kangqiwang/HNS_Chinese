562fbe822a583fdf9ce744bf9df59af772d7fcfe
Kang Wang <kangqiwang@outlook.com>
2022-10-25 15:37:43 +0100

    update nhs chinese

--- src/main/java/com/kang/medicalwebapplication/MedicalWebApplication.java
+++ src/main/java/com/kang/medicalwebapplication/MedicalWebApplication.java
@@ -3,13 +3,16 @@ package com.kang.medicalwebapplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 @EnableAutoConfiguration
+@EnableCaching
+
 public class MedicalWebApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MedicalWebApplication.class, args);
+		
 	}
-
 }
