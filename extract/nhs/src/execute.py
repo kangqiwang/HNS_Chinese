b958818f486f5a528ba1331beeccdd9ebb17ca22
kang wang <kangqiwang@outlook.com>
2024-11-07 13:48:45 +0000

    update extract

--- extract/nhs/src/execute.py
+++ extract/nhs/src/execute.py
@@ -7,7 +7,7 @@ if __name__ =="__main__":
     logging.basicConfig(stream=sys.stdout,level=20)
     base_url = "https://api.nhs.uk/conditions/"
     # config_dict = env.copy()
-    api_key=""
+    api_key="2b4002aa80684907970da3ca540b6712"
     out_dir="conditions"
     logging.info("Getting all nhs conditions")
     nhsAPI=ConditionsAPI(base_url,api_key,out_dir)
